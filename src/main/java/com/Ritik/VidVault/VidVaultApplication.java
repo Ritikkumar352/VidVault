e5de3f2b8a0233bb02eb43e34a59647900caae7d
ritikkumar352 <krritik2002@gmail.com>
2025-03-10 19:31:57 +0530

    setup GCP

--- src/main/java/com/Ritik/VidVault/VidVaultApplication.java
+++ src/main/java/com/Ritik/VidVault/VidVaultApplication.java
@@ -1,5 +1,6 @@
 package com.Ritik.VidVault;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class VidVaultApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VidVaultApplication.class, args);
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", dotenv.get("GOOGLE_APPLICATION_CREDENTIALS"));
+
 	}
 
 }
