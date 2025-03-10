76b3124e7dc0181bea85d798a50c413d1d045d89
ritikkumar352 <krritik2002@gmail.com>
2025-03-10 13:01:40 +0530

    Video (entity) done-- table auto generated

--- src/main/java/com/Ritik/VidVault/entity/Video.java
+++ src/main/java/com/Ritik/VidVault/entity/Video.java
@@ -13,11 +13,12 @@ public class Video {
 
     private String title;
     private String description;
-    @Column(name = "uploadedAt")
     private String url;
+
+    @Column(name = "uploadedAt")
     private LocalDateTime uploadedAt;
     // first complete these ...above
-    private int duration;   // TODO -- format to HH:MM:SS to display
+    private int duration;   // TODO -- format to HH:MM:SS to displayadd  , it's in sec ig
     private int size;
     private String format;
 
@@ -26,7 +27,67 @@ public class Video {
         uploadedAt = LocalDateTime.now();
     }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(LocalDateTime uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
 }
